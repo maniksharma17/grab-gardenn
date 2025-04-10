49ea1493c0a4dcc0a64ebc5fef32690ac194b6b3
Manik Sharma <maniksharma.rke@gmail.com>
2025-04-10 21:03:34 +0530

    latest

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,20 +1,35 @@
 import { clsx, type ClassValue } from 'clsx';
+import { useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-import { useEffect, useState } from "react";
+export const localStorageEffect = (key: string) => 
+  ({ setSelf, onSet }: any) => {
+    if (typeof window === "undefined") return;
 
-export function useWindowWidth() {
-  const [width, setWidth] = useState<number>(typeof window !== "undefined" ? window.innerWidth : 0);
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
 
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+    onSet((newValue: any, _: any, isReset: boolean) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
 
-  return width;
-}
+  export function useWindowWidth() {
+    const [width, setWidth] = useState<number>(typeof window !== "undefined" ? window.innerWidth : 0);
+  
+    useEffect(() => {
+      const handleResize = () => setWidth(window.innerWidth);
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+  
+    return width;
+  }
