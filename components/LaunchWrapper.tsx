92cb7769bfc850f8987ced6c9455e90bbfbcac84
Manik Sharma <maniksharma.rke@gmail.com>
2025-04-10 12:59:40 +0530

    new

--- components/LaunchWrapper.tsx
+++ components/LaunchWrapper.tsx
@@ -7,7 +7,7 @@ const LaunchWrapper = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     // Create launch time as UTC timestamp (12 PM IST = 6:30 AM UTC)
-    const launchTime = new Date(Date.UTC(2025, 3, 10, 7, 30)); 
+    const launchTime = new Date(Date.UTC(2025, 3, 10, 7, 33)); 
 
     const updateCountdown = () => {
       const now = new Date();
