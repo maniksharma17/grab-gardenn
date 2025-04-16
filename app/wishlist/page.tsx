c8d46f7ab5f2cf5aa996eb204e6eeb944b837707
Manik Sharma <maniksharma.rke@gmail.com>
2025-04-17 04:31:50 +0530

    new

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { useRecoilValue } from 'recoil';
 import { userState } from '@/store/atoms/user';
