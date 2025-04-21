e6600b6b1a7681708f7e1acd8a3eebd073a2fab3
Manik Sharma <maniksharma.rke@gmail.com>
2025-04-21 10:43:29 +0530

    files added

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -562,7 +562,7 @@ export default function ProductPage() {
         </div>
       </div>
       {/* Reviews Section */}
-      <div className="mt-20 border-t pt-10">
+      <div className="container mt-20 border-t pt-10">
         <h2 className="text-2xl font-semibold mb-4">Customer Reviews</h2>
 
         {/* Review Input */}
