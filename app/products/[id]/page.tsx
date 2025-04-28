2e3a5992d57730753c97073f3e56654dd9fa541b
Manik Sharma <maniksharma.rke@gmail.com>
2025-04-29 00:07:12 +0530

    files added

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -536,7 +536,7 @@ export default function ProductPage() {
               </>
             ) : (
               // Zoomed Image Section
-              <div className="max-md:hidden w-full h-[500px] relative">
+              <div className="max-md:hidden w-full h-[700px] relative">
                 <div
                   className="absolute w-[100%] h-[100%] shadow-md border border-gray-300 border-dashed"
                   style={{
