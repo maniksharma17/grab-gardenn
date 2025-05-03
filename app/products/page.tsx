01a9539dcc9c8d86d43aef94bf531c911dd3f2c1
Manik Sharma <maniksharma.rke@gmail.com>
2025-05-04 03:55:33 +0530

    new

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -39,7 +39,7 @@ export default function ProductsPage() {
 
   useEffect(()=>{
     if(!user) return;
-    if(user?._id && user?.phone == ""){
+    if(user.phone === ""){
       router.push("/complete-profile");
     }
   }, [user, router])
