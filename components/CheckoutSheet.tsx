cb1009e6f06ead53a9e8ca6e8d0d388e11cfdb4d
Manik Sharma <maniksharma.rke@gmail.com>
2025-05-06 04:02:19 +0530

    new

--- components/CheckoutSheet.tsx
+++ components/CheckoutSheet.tsx
@@ -643,7 +643,7 @@ export const CheckoutSheet = ({
           )}
 
           {/* Address Selection */}
-          <div className="space-y-2 my-8 border-t">
+          <div className="space-y-2 my-12 py-8 border-t">
             <h3 className="text-lg font-semibold">Shipping Address</h3>
             <Select
               onValueChange={(val) => {
