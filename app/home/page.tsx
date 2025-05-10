54d275d47fb6527284e634495e4b3998901299b9
Manik Sharma <maniksharma.rke@gmail.com>
2025-05-10 22:15:58 +0530

    new

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -52,19 +52,6 @@ export default function Home() {
       heading: "The Taste of Culture, The Purity of Nature",
       text: "Experience Himalayan ingredients passed down through generations—pure, local, and full of natural nourishment.",
     },
-
-    {
-      src: "https://grabgardenn-storage.s3.ap-south-1.amazonaws.com/banners/home-banner-8.png",
-      href: "/products",
-      heading: "Nature’s Finest, Packed with Care",
-      text: "Every product is a promise of purity, nutrition, and the rich heritage of the mountains.",
-    },
-    {
-      src: "https://grabgardenn-storage.s3.ap-south-1.amazonaws.com/banners/home-banner-6.jpeg",
-      href: "/products",
-      heading: "The Taste of Culture, The Purity of Nature",
-      text: "Experience Himalayan ingredients passed down through generations—pure, local, and full of natural nourishment.",
-    },
   ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
