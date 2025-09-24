02822d092bf5e3c02b7aab240e2cc6bbf78bc5f5
Manik Sharma <maniksharma.rke@gmail.com>
2025-09-25 04:36:31 +0530

    file

--- components/CartSheet.tsx
+++ components/CartSheet.tsx
@@ -356,7 +356,7 @@ export const CartSheet = () => {
 
             {/* Suggestions */}
             {suggestions.length > 0 && (
-              <div className="mt-2 max-md:py-4 lg:mt-0 border-t lg:border-t-0 lg:border-l lg:pl-4 overflow-y-auto md:max-h-[75vh]">
+              <div className="mt-2 max-md:py-4 lg:mt-0 border-t lg:border-t-0 lg:border-l lg:pl-4 overflow-y-scroll h-auto md:max-h-[75vh]">
                 <h4 className="text-sm font-semibold mb-3">
                   You may also like
                 </h4>
